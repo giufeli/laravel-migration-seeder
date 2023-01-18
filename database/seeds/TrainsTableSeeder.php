84015599c878e45978d226dd2d614609fdc6e12d
giufeli <giuliofelicetti99@gmail.com>
2023-01-18 16:45:28 +0100

    Commit

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -14,11 +14,18 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $element = [];
 
-        foreach ($element as $trains) {
+        for ($i = 0; $i < 5; $i++) {
             $train = new trains();
+            $train->company = $faker->company;
+            $train->departure_station = $faker->city;
+            $train->arrival_station= $faker->city;
+            $train->departure_time = $faker->dateTime('now');
+            $train->arrival_time = $faker->dateTime('now');
             $train->train_code = rand(25000, 70000);
+            $train->number_of_carriages= rand(2, 15);
+            $train->in_time = $faker->boolean;
+            $train->deleted = $faker->boolean;
             $train->save();
         }
     }
